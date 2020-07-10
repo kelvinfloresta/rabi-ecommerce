69abd0a7425c1a8dce0539222b0bae47ec7f83e7
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-10 00:21:51 -0300

    refactor: migrating ProductGateway to use KnexInstance

--- src/adapters/gateways/Product/ProductKnexAdapter.gateway.ts
+++ src/adapters/gateways/Product/ProductKnexAdapter.gateway.ts
@@ -1,24 +1,24 @@
 import Product from 'src/entities/Product.entity';
-import db from 'src/adapters/database/Database.adapter';
+import KnexInstance from 'src/frameworks/database/knex/knex-adapter.framework';
 import IProductGateway from './IProduct.gateway';
 
-export default class ProductGatewayKnexAdapter implements IProductGateway {
-  private db = db(ProductGatewayKnexAdapter.tableName);
+export default class ProductGatewayKnexAdapter extends KnexInstance implements IProductGateway {
+  public tableName = 'products';
 
   public static tableName = 'products';
 
   async save(input: Product): Promise<Product> {
-    const [result] = await this.db.insert(input).returning(['id', 'name', 'description', 'price', 'disabled']);
+    const [result] = await this.instance.insert(input).returning(['id', 'name', 'description', 'price', 'disabled']);
     return result;
   }
 
   async patch(id: string, input: Partial<Omit<Product, 'id'>>): Promise<Product> {
-    const [result] = await this.db.update(input).where({ id }).returning(['id', 'name', 'description', 'price', 'disabled']);
+    const [result] = await this.instance.update(input).where({ id }).returning(['id', 'name', 'description', 'price', 'disabled']);
     return result;
   }
 
   async delete(id: string): Promise<boolean> {
-    const result = await this.db.update({ deleted: true }).where({ id });
+    const result = await this.instance.where({ id }).update({ deletedAt: new Date() });
     return result > 0;
   }
 }
