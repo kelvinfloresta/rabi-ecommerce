58330182511608dae5daa97354f7b522629b22a6
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-10 00:15:36 -0300

    feat: add rebuildDatabase function

--- src/frameworks/database/knex/knex-helper.framework.ts
+++ src/frameworks/database/knex/knex-helper.framework.ts
@@ -1,4 +1,6 @@
 import Knex from 'knex';
+import config from 'src/config';
+import knexFile from './knexfile';
 
 export function PostgresUUIDV4(knex: Knex): Knex.Raw {
   return knex.raw('uuid_generate_v4()');
@@ -21,4 +23,24 @@ export async function addTimestamp(
 
 export function addSoftDelete(table: Knex.CreateTableBuilder): void {
   table.timestamp('deletedAt', { useTz: false }).nullable().defaultTo(null);
+}
+
+export async function rebuildDatabase() {
+  const { envName } = config;
+  const databaseConfig = knexFile[envName];
+  const params = {
+    client: databaseConfig.client,
+    connection: {
+      host: databaseConfig.connection.host,
+      user: databaseConfig.connection.user,
+      password: databaseConfig.connection.password,
+      charset: databaseConfig.connection.charset,
+    },
+  };
+
+  const knex = Knex(params);
+
+  await knex.raw(`DROP DATABASE IF EXISTS ${databaseConfig.connection.database}`);
+  await knex.raw(`CREATE DATABASE ${databaseConfig.connection.database}`);
+  await knex.destroy();
 }
