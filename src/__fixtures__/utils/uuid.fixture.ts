64321c699fb60c5ac2533f7a29884faa578802f8
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-09 21:24:01 -0300

    chore: add uuid fixture

---
+++ src/__fixtures__/utils/uuid.fixture.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 } from 'uuid';
+
+// eslint-disable-next-line import/prefer-default-export
+export const uuidv4: () => string = v4;
