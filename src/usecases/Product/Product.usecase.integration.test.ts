48ce39feca34857152a287db3604613e0137e3c4
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-09 21:26:33 -0300

    test: add ProductCase patch tests

--- src/usecases/Product/Product.usecase.integration.test.ts
+++ src/usecases/Product/Product.usecase.integration.test.ts
@@ -1,5 +1,5 @@
 import { closeDatabase } from 'src/adapters/database/Database.adapter';
-import { buildProductFixture } from 'src/__fixtures__/product.fixture';
+import { buildProductFixture, createProductFixture } from 'src/__fixtures__/product.fixture';
 import ProductCaseFactory from './ProductFactory.usecase';
 
 afterAll(async () => {
@@ -13,6 +13,48 @@ describe('Product Case', () => {
       const sut = ProductCaseFactory();
       const result = await sut.save(product);
       expect(result).toMatchObject(product);
+    });
+  });
+
+  describe('patch', () => {
+    it('Should patch name', async () => {
+      const sut = ProductCaseFactory();
+      const product = await createProductFixture({ name: 'cake' }, sut);
+      const newName = 'cupcake';
+      const result = await sut.patch(product.id, { name: newName });
+      expect(result.name).toBe(newName);
+    });
+
+    it('Should patch description', async () => {
+      const sut = ProductCaseFactory();
+      const product = await createProductFixture({ price: 10 }, sut);
+      const newDescription = 'A lovely product';
+      const result = await sut.patch(product.id, { description: newDescription });
+      expect(result.description).toBe(newDescription);
+    });
+
+    it('Should patch price', async () => {
+      const sut = ProductCaseFactory();
+      const product = await createProductFixture({ price: 10 }, sut);
+      const newPrice = 10.99;
+      const result = await sut.patch(product.id, { price: newPrice });
+      expect(result.price).toBe(newPrice);
+    });
+
+    it('Should patch disabled', async () => {
+      const sut = ProductCaseFactory();
+      const product = await createProductFixture({ disabled: true }, sut);
+      const result = await sut.patch(product.id, { disabled: false });
+      expect(result.disabled).toBeFalsy();
+    });
+
+    it('Should not patch the other product values', async () => {
+      const sut = ProductCaseFactory();
+      const product = await createProductFixture({ name: 'banana', price: 1 }, sut);
+      const result = await sut.patch(product.id, { name: 'A rare banana', price: 999 });
+      const fieldsNotChanged = { description: product.description, disabled: product.disabled };
+      const fieldsNotpatchd = { description: result.description, disabled: result.disabled };
+      expect(fieldsNotChanged).toMatchObject(fieldsNotpatchd);
     });
   });
 });
