35356ade77425e587562328c1b569cfa94e99dde
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-11 01:52:46 -0300

    feat: add create company migration script

---
+++ src/frameworks/database/knex/migrations/20200711001223_create_company.ts
@@ -0,0 +1,21 @@
+import Knex from 'knex';
+import CompanyGatewayKnexAdapter from 'src/adapters/gateways/Company/CompanyKnexAdapter.gateway';
+import DocumentType from 'src/entities/enums/DocumentType.enum';
+import { PostgresUUIDV4, addTimestamp } from '../knex-migration.framework';
+
+const { tableName } = CompanyGatewayKnexAdapter;
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema
+    .createTable(tableName, (table) => {
+      table.uuid('id').primary().defaultTo(PostgresUUIDV4(knex));
+      table.string('name').notNullable();
+      addTimestamp(table, knex, tableName);
+      table.string('documentNumber').notNullable();
+      table.enum('documentType', [DocumentType.CPF, DocumentType.CNPJ]);
+    });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.dropTable(tableName);
+}
