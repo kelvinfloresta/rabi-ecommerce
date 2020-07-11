654c86e945c891131c1276d9b8407e39a82cf9c1
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-11 01:53:55 -0300

    feat: add make functions for KnexAdapter

--- src/frameworks/database/knex/knex-adapter.framework.ts
+++ src/frameworks/database/knex/knex-adapter.framework.ts
@@ -1,11 +1,38 @@
 import db from 'src/adapters/database/Database.adapter';
 
-export default abstract class KnexInstance {
+export default abstract class KnexBaseGateway {
   private db = db;
+
+  abstract tableName: string;
 
   get instance() {
     return this.db(this.tableName);
   }
 
-  abstract tableName: string;
+  static makeGet<T>() {
+    return async function get(id: string): Promise<T> {
+      return this.instance.where({ id }).first();
+    };
+  }
+
+  static makeSave<T>(returning: '*' | Array<keyof T>) {
+    return async function save(input: T): Promise<T> {
+      const [result] = await this.instance.insert(input).returning(returning);
+      return result;
+    };
+  }
+
+  static makePatch<T>(returning: '*' | Array<keyof T>) {
+    return async function patch(id: string, input: Partial<Omit<T, 'id'>>): Promise<T> {
+      const [result] = await this.instance.update(input).where({ id }).returning(returning);
+      return result;
+    };
+  }
+
+  static makeLogicDelete() {
+    return async function logicDelete(id: string): Promise<boolean> {
+      const result = await this.instance.where({ id }).update({ deletedAt: new Date() });
+      return result > 0;
+    };
+  }
 }
