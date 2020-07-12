0bc98a5aad0b46ec3ceba3390f381225134adcce
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-11 22:28:29 -0300

    refactor: migrating make methods to super class

--- src/adapters/gateways/Product/ProductKnexAdapter.gateway.ts
+++ src/adapters/gateways/Product/ProductKnexAdapter.gateway.ts
@@ -1,17 +1,10 @@
-import Product from 'src/entities/Product.entity';
 import KnexBaseGateway from 'src/frameworks/database/knex/knex-adapter.framework';
+import Product from 'src/entities/Product.entity';
 import IProductGateway from './IProduct.gateway';
 
-export default class ProductGatewayKnexAdapter extends KnexBaseGateway implements IProductGateway {
+export default class ProductGatewayKnexAdapter extends KnexBaseGateway<Product>
+  implements IProductGateway {
   public tableName = ProductGatewayKnexAdapter.tableName;
 
   public static tableName = 'products';
-
-  get = KnexBaseGateway.makeGet<Product>();
-
-  save = ProductGatewayKnexAdapter.makeSave<Product>('*');
-
-  patch = KnexBaseGateway.makePatch<Product>(['id', 'name', 'description', 'price', 'disabled']);
-
-  delete = KnexBaseGateway.makeLogicDelete();
 }
--- src/frameworks/database/knex/knex-adapter.framework.ts
+++ src/frameworks/database/knex/knex-adapter.framework.ts
@@ -1,6 +1,6 @@
 import db from 'src/adapters/database/Database.adapter';
 
-export default abstract class KnexBaseGateway {
+export default abstract class KnexBaseGateway<T> {
   private db = db;
 
   abstract tableName: string;
@@ -9,30 +9,20 @@ export default abstract class KnexBaseGateway {
     return this.db(this.tableName);
   }
 
-  static makeGet<T>() {
-    return async function get(id: string): Promise<T> {
-      return this.instance.where({ id }).first();
-    };
+  async get(id: string): Promise<T> {
+    return this.instance.where({ id }).first();
   }
 
-  static makeSave<T>(returning: '*' | Array<keyof T>) {
-    return async function save(input: T): Promise<T> {
-      const [result] = await this.instance.insert(input).returning(returning);
-      return result;
-    };
+  async save(input: T): Promise<void> {
+    await this.instance.insert(input);
   }
 
-  static makePatch<T>(returning: '*' | Array<keyof T>) {
-    return async function patch(id: string, input: Partial<Omit<T, 'id'>>): Promise<T> {
-      const [result] = await this.instance.update(input).where({ id }).returning(returning);
-      return result;
-    };
+  async patch(id: string, input: Partial<Omit<T, 'id'>>): Promise<void> {
+    await this.instance.update(input).where({ id });
   }
 
-  static makeLogicDelete() {
-    return async function logicDelete(id: string): Promise<boolean> {
-      const result = await this.instance.where({ id }).update({ deletedAt: new Date() });
-      return result > 0;
-    };
+  async logicDelete(id: string): Promise<boolean> {
+    const result = await this.instance.where({ id }).update({ deletedAt: new Date() });
+    return result > 0;
   }
 }
