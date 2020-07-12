5aeb0a16e2f6e7432368aa4c89ff79c5970bd061
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-11 22:18:47 -0300

    feat: add company relation to product in migration script

--- src/frameworks/database/knex/migrations/20200711010320_create_product.ts
+++ src/frameworks/database/knex/migrations/20200711010320_create_product.ts
@@ -1,20 +1,24 @@
 import * as Knex from 'knex';
 import ProductGatewayKnexAdapter from 'src/adapters/gateways/Product/ProductKnexAdapter.gateway';
+import CompanyGatewayKnexAdapter from 'src/adapters/gateways/Company/CompanyKnexAdapter.gateway';
 import { PostgresUUIDV4, addSoftDelete, addTimestamp } from '../knex-migration.framework';
 
 const { tableName } = ProductGatewayKnexAdapter;
+const { tableName: companyTable } = CompanyGatewayKnexAdapter;
 
 export async function up(knex: Knex): Promise<any> {
-  return knex.schema
-    .createTable(tableName, (table) => {
-      table.uuid('id').primary().defaultTo(PostgresUUIDV4(knex));
-      table.string('name').notNullable();
-      addSoftDelete(table);
-      addTimestamp(table, knex, tableName);
-      table.decimal('price').notNullable();
-      table.boolean('disabled').defaultTo(false).notNullable();
-      table.text('description').nullable();
-    });
+  return knex.schema.createTable(tableName, (table) => {
+    table.uuid('id').primary().defaultTo(PostgresUUIDV4(knex));
+    table.string('name').notNullable();
+    addSoftDelete(table);
+    addTimestamp(table, knex, tableName);
+    table.decimal('price').notNullable();
+    table.boolean('disabled').defaultTo(false).notNullable();
+    table.text('description').nullable();
+    table.uuid('companyId').notNullable();
+
+    table.foreign('companyId').references('id').inTable(companyTable);
+  });
 }
 
 export async function down(knex: Knex): Promise<any> {
