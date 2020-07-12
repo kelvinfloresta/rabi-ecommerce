a618cf7a00be8048446ff6e9dfe38e7174330866
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-12 17:56:43 -0300

    fix: removing old HOC functions from Company gateway

--- src/adapters/gateways/Company/CompanyKnexAdapter.gateway.ts
+++ src/adapters/gateways/Company/CompanyKnexAdapter.gateway.ts
@@ -2,12 +2,9 @@ import Company from 'src/entities/Company.entity';
 import KnexBaseGateway from 'src/frameworks/database/knex/knex-adapter.framework';
 import ICompanyGateway from './ICompany.gateway';
 
-export default class CompanyGatewayKnexAdapter extends KnexBaseGateway implements ICompanyGateway {
+export default class CompanyGatewayKnexAdapter extends KnexBaseGateway<Company>
+  implements ICompanyGateway {
   public tableName = 'companies';
 
   public static tableName = 'companies';
-
-  get = KnexBaseGateway.makeGet<Company>();
-
-  save = KnexBaseGateway.makeSave<Company>('*');
 }
