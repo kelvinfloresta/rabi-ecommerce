925d5aab76d549897e83de8ee707b4f5a1d4492e
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-08-01 15:51:41 -0300

    fix: Cart changeQuantity gateway

--- src/adapters/gateways/Cart/CartKnexAdapter.gateway.ts
+++ src/adapters/gateways/Cart/CartKnexAdapter.gateway.ts
@@ -1,17 +1,17 @@
-import KnexRepositoryHelper from 'src/frameworks/database/knex/knex-adapter.framework';
 import { IChangeQuantityCartCaseInput } from 'src/usecases/Cart/ICart.usecase';
+import KnexRepositoryHelper from 'src/frameworks/database/knex/knex-adapter.framework';
 import CartItem from 'src/entities/CartItem.entity';
 import ICartGateway from './ICart.gateway';
 
 export default class CartGatewayKnexAdapter implements ICartGateway {
   public static readonly tableName = 'CartItem';
 
-  constructor(private cartItemRepository: KnexRepositoryHelper<CartItem>) {}
+  constructor(private repository: KnexRepositoryHelper<CartItem>) {}
 
   async changeQuantity(input: IChangeQuantityCartCaseInput): Promise<boolean> {
-    const result = await this.cartItemRepository.updateByFilter(
-      { quantity: input.quantity },
-      { userId: input.userId, productId: input.productId }
+    const result = await this.repository.updateByFilter(
+      { userId: input.userId, productId: input.productId },
+      { quantity: input.quantity }
     );
 
     return result > 0;
