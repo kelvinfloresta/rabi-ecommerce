d4ef903b2575e5df32002b6949842ec194722c7a
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-08-02 18:56:59 -0300

    feat: add AuthCase

---
+++ src/usecases/Auth/Auth.usecase.integration.test.ts
@@ -0,0 +1,74 @@
+import Encrypt from 'src/utils/Encrypt.util';
+import { createUserFixture } from 'src/__fixtures__/user.fixture';
+import { createCompanyFixture } from 'src/__fixtures__/company.fixture';
+import { truncateTable } from 'src/adapters/database/Database.adapter';
+import UserGatewayKnexAdapter from 'src/adapters/gateways/User/UserKnexAdapter.gateway';
+import NotAuthorized from 'src/utils/errors/NotAuthorized.error';
+import AuthCase from './Auth.usecase';
+import UserCaseFactory from '../User/UserFactory.usecase';
+
+beforeEach(async () => {
+  await truncateTable(UserGatewayKnexAdapter.tableName);
+});
+function makeSut() {
+  return new AuthCase('secret', UserCaseFactory.singleton, Encrypt);
+}
+
+describe('File: Auth.usecase.ts', () => {
+  describe('login', () => {
+    it('Should return token if email and password is correct', async () => {
+      const { id: companyId } = await createCompanyFixture();
+      const email = 'email';
+      const password = 'password';
+      await createUserFixture({ password, email, companyId });
+
+      const sut = makeSut();
+      const token = await sut.login({ email, password });
+      expect(token).toBeTruthy();
+    });
+
+    it('Should reject if email not exists', async () => {
+      const emailNotExists = 'email';
+      const anyPassword = 'password';
+
+      const sut = makeSut();
+      const promise = sut.login({ email: emailNotExists, password: anyPassword });
+      expect(promise).rejects.toThrowError(NotAuthorized);
+    });
+
+    it('Should reject if password is wrong', async () => {
+      const { id: companyId } = await createCompanyFixture();
+      const email = 'email';
+      const password = 'password';
+      await createUserFixture({ password, email, companyId });
+
+      const wrongPassword = 'wrongPassword';
+      const sut = makeSut();
+      const promise = sut.login({ email, password: wrongPassword });
+      expect(promise).rejects.toThrowError(NotAuthorized);
+    });
+  });
+
+  describe('authenticate', () => {
+    it('Should not reject if token is valid', async () => {
+      const { id: companyId } = await createCompanyFixture();
+      const email = 'email';
+      const password = 'password';
+      await createUserFixture({ password, email, companyId });
+
+      const sut = makeSut();
+      const token = await sut.login({ email, password });
+
+      const lazyAuthenticate = () => sut.authenticate(token);
+      expect(lazyAuthenticate).not.toThrow();
+    });
+
+    it('Should reject if token is not valid', async () => {
+      const sut = makeSut();
+      const invalidToken =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.9EmJDfAi6fqc_FoO3yD77spdi7QZX-s9VzN89vOaOp0';
+      const lazyAuthenticate = () => sut.authenticate(invalidToken);
+      expect(lazyAuthenticate).toThrow();
+    });
+  });
+});
---
+++ src/usecases/Auth/Auth.usecase.ts
@@ -0,0 +1,38 @@
+import * as jwt from 'jsonwebtoken';
+import User from 'src/entities/User.entity';
+import NotAuthorized from 'src/utils/errors/NotAuthorized.error';
+import Encrypt from 'src/utils/Encrypt.util';
+import UserCase from '../User/User.usecase';
+
+export default class AuthCase {
+  private readonly jwt = jwt;
+
+  constructor(
+    private readonly secretKey: string,
+    private readonly userCase: UserCase,
+    private readonly encrypt: typeof Encrypt
+  ) {}
+
+  private sign(email: string) {
+    return this.jwt.sign({ email }, this.secretKey);
+  }
+
+  public authenticate(token: string) {
+    this.jwt.verify(token, this.secretKey);
+  }
+
+  public async login(credentials: Pick<User, 'email' | 'password'>) {
+    const user = await this.userCase.findByEmail(credentials.email);
+    if (!user) {
+      throw new NotAuthorized('Email not found');
+    }
+
+    const isValid = this.encrypt.compare(user.password, credentials.password);
+    if (!isValid) {
+      throw new NotAuthorized('Wrong password');
+    }
+
+    const token = this.sign(user.email);
+    return token;
+  }
+}
