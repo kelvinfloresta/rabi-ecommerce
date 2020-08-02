8db1625340c2b218c7522d45b67ed4236977387a
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-08-02 15:24:57 -0300

    feat: add get cart usecase

--- src/usecases/Cart/Cart.usecase.ts
+++ src/usecases/Cart/Cart.usecase.ts
@@ -1,10 +1,15 @@
 import ICartGateway from 'src/adapters/gateways/Cart/ICart.gateway';
 import CartItem from 'src/entities/CartItem.entity';
+import { IGetCartCaseInput } from './ICart.usecase';
 
 export default class CartCase {
   constructor(private CartGateway: ICartGateway) {}
 
   async add(input: CartItem): Promise<boolean> {
     return this.CartGateway.add(input);
+  }
+
+  async get(input: IGetCartCaseInput): Promise<CartItem> {
+    return this.CartGateway.get(input);
   }
 }
---
+++ src/usecases/Cart/ICart.usecase.ts
@@ -0,0 +1,4 @@
+export interface IGetCartCaseInput {
+  readonly productId: string;
+  readonly userId: string;
+}
