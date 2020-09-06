116f829275e64c737d77eafb75e47d6fc93bc17d
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-09-06 17:58:15 -0300

    feat: add User validator

---
+++ src/adapters/controllers/User/UserValidator.controller.ts
@@ -0,0 +1,56 @@
+import { JSONSchema7 } from 'json-schema';
+
+export const UserSaveSchema: JSONSchema7 = {
+  type: 'object',
+  properties: {
+    body: {
+      type: 'object',
+      properties: {
+        email: {
+          type: 'string',
+        },
+        password: {
+          type: 'string',
+        },
+        companyId: {
+          type: 'string',
+        },
+        name: {
+          type: 'string',
+        },
+        documentNumber: {
+          type: 'string',
+        },
+        documentType: {
+          type: 'string',
+        },
+      },
+      required: ['email', 'password'],
+    },
+  },
+};
+
+export const getSchema: JSONSchema7 = {
+  type: 'object',
+  properties: {
+    email: {
+      type: 'string',
+    },
+    password: {
+      type: 'string',
+    },
+    companyId: {
+      type: 'string',
+    },
+    name: {
+      type: 'string',
+    },
+    documentNumber: {
+      type: 'string',
+    },
+    documentType: {
+      type: 'string',
+    },
+  },
+  required: ['email', 'password'],
+};
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { fastify } from 'fastify';
 import UserCaseFactory from './usecases/User/UserFactory.usecase';
 import FastifyAdapter from './adapters/controllers/FastifyAdapter.controller';
-import UserController from './adapters/controllers/User.controller';
+import UserController from './adapters/controllers/User/User.controller';
 import RouteConfig from './adapters/controllers/RouteConfig';
+import AJVValidatorAdapter from './adapters/Validator/AJVValidator.validator';
 
 const app = fastify({
   logger: true,
 });
 
 const routes: RouteConfig = new FastifyAdapter(app);
-const userController = new UserController(UserCaseFactory.singleton);
+const userController = new UserController(UserCaseFactory.singleton, AJVValidatorAdapter);
 routes.appendController(userController);
 
 app.listen(3000, '0.0.0.0', (err, address) => {
