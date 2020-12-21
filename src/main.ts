25bc4111d629ba68b26b01d29c7a63135a3fc580
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-21 20:22:51 -0300

    chore(main): add controllers auth, company, category

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { UserCaseFactory } from './usecases/User/UserFactory.usecase';
 import { UserController } from './adapters/controllers/User/User.controller';
 import { RouteFactory } from './adapters/controllers/routes/RouteFactory.route';
 import { FastifyRouterFactoryAdapter } from './adapters/controllers/routes/FastifyRouterFactoryAdapter.route';
+import { AuthController } from './adapters/controllers/Auth/Auth.controller';
+import { CategoryController } from './adapters/controllers/Category/Category.controller';
+import { AuthCaseFactory } from './usecases/Auth/AuthFactory.usecase';
+import { CategoryCaseFactory } from './usecases/Category/CategoryFactory.usecase';
+import { CompanyController } from './adapters/controllers/Company/Company.controller';
+import { CompanyCaseFactory } from './usecases/Company/CompanyFactory.usecase';
 
 const app = fastify({
   logger: true,
@@ -10,5 +16,16 @@ const app = fastify({
 
 const routes: RouteFactory = new FastifyRouterFactoryAdapter(app);
 const userController = new UserController(UserCaseFactory.singleton);
+const authController = new AuthController(AuthCaseFactory.singleton);
+const companyController = new CompanyController(CompanyCaseFactory());
+const categoryController = new CategoryController(
+  CategoryCaseFactory.singleton,
+  AuthCaseFactory.singleton
+);
+
 routes.addController(userController);
+routes.addController(authController);
+routes.addController(categoryController);
+routes.addController(companyController);
+
 routes.start();
