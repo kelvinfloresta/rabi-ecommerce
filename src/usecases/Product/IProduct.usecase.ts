5a3ff6e1ca324a589b5de6ef852954b0067db90d
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-21 20:16:21 -0300

    refactor(IProductCase): change ISaveProductCaseInput to be predictable

--- src/usecases/Product/IProduct.usecase.ts
+++ src/usecases/Product/IProduct.usecase.ts
@@ -1,8 +1,14 @@
 import { Product } from 'src/entities/Product.entity';
 import { ITimeStamp, ILogicDelete } from 'src/entities/IEntity';
 
-export interface ISaveProductCaseInput
-  extends Omit<Product, 'id' | keyof ITimeStamp | keyof ILogicDelete> {}
+export interface ISaveProductCaseInput {
+  readonly name: string;
+  readonly price: number;
+  readonly description?: string;
+  readonly disabled: boolean;
+  readonly categoryId: string | null;
+  readonly companyId: string;
+}
 
 export interface IPatchProductCaseInput
   extends Omit<Partial<Product>, 'companyId' | keyof ITimeStamp | keyof ILogicDelete> {
