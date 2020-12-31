b29f52a8daeeef3b1f6f02acc6d32449bab78fb2
kelvinfloresta <kelvinf.contato@gmail.com>
2020-12-30 23:17:11 -0300

    chore(ProductGatewayKnexAdapter): change paginate to list

--- src/adapters/gateways/Product/ProductKnexAdapter.gateway.ts
+++ src/adapters/gateways/Product/ProductKnexAdapter.gateway.ts
@@ -2,14 +2,26 @@ import { Product } from 'src/entities/Product.entity';
 import {
   ISaveProductCaseInput,
   IPatchProductCaseInput,
-  IPaginateProductCaseInput,
+  IListProductCaseInput,
 } from 'src/usecases/Product/IProduct.usecase';
 import { KnexRepositoryHelper } from 'src/frameworks/database/knex/knex-repository-helper.framework';
-import { IPaginationParams } from 'src/usecases/IPaginate';
+import { TableName } from 'src/adapters/database/Database.adapter';
 import { IProductGateway } from './IProduct.gateway';
 import { ICommonCompanyFilter } from '../IGateway';
 
 export class ProductGatewayKnexAdapter implements IProductGateway {
+  private companyId = `${TableName.product}.companyId`;
+
+  private productId = `${TableName.product}.id`;
+
+  private categoryId = `${TableName.category}.id`;
+
+  private categoryName = `${TableName.category}.name as categoryName`;
+
+  private productName = `${TableName.product}.name`;
+
+  private productDescription = `${TableName.product}.description`;
+
   constructor(private repository: KnexRepositoryHelper<Product>) {}
 
   async hardDelete(filter: ICommonCompanyFilter): Promise<boolean> {
@@ -29,7 +41,20 @@ export class ProductGatewayKnexAdapter implements IProductGateway {
     return this.repository.save(input);
   }
 
-  async paginate(filter: IPaginateProductCaseInput, paginate: IPaginationParams) {
-    return this.repository.paginateByFilter(filter, paginate);
+  async list(filter: IListProductCaseInput): Promise<Product[]> {
+    return this.repository.instance
+      .select(
+        this.companyId,
+        this.productId,
+        this.categoryName,
+        this.categoryName,
+        this.productName,
+        this.productDescription,
+        'categoryId',
+        'active',
+        'price'
+      )
+      .leftJoin(TableName.category, this.categoryId, 'categoryId')
+      .where(this.companyId, filter.companyId);
   }
 }
