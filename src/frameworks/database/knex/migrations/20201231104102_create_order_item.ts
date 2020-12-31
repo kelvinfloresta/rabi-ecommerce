865d6d6f518f3429314d12b75f762669f789a1a9
kelvinfloresta <kelvinf.contato@gmail.com>
2020-12-31 07:58:48 -0300

    chore: add Order Item migration

---
+++ src/frameworks/database/knex/migrations/20201231104102_create_order_item.ts
@@ -0,0 +1,24 @@
+import Knex from 'knex';
+import { TableName } from 'src/adapters/database/Database.adapter';
+import { addTimestamp } from '../knex-migration.framework';
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable(TableName.orderItem, (table) => {
+    addTimestamp(table, knex, TableName.orderItem);
+
+    table.uuid('orderId').nullable();
+    table.foreign('orderId').references('id').inTable(TableName.order).onDelete('CASCADE');
+
+    table.uuid('productId').nullable();
+    table.foreign('productId').references('id').inTable(TableName.product);
+    table.string('productName').notNullable();
+
+    table.decimal('quantity').notNullable();
+
+    table.decimal('total').notNullable();
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(TableName.orderItem);
+}
