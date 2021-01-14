4995445f48512416d70c3b88f87947b484cab0b3
kelvinfloresta <kelvinf.contato@gmail.com>
2021-01-14 19:31:09 -0300

    refactor(OrderMigration): remove duplicated interface

--- src/frameworks/database/knex/migrations/20201231030658_create_orders.ts
+++ src/frameworks/database/knex/migrations/20201231030658_create_orders.ts
@@ -2,20 +2,6 @@ import * as Knex from 'knex';
 import { TableName } from 'src/adapters/database/Database.adapter';
 import { addTimestamp, PostgresUUIDV4 } from '../knex-migration.framework';
 
-export interface IOrderItemBusinessData {
-  readonly productId: string;
-  readonly productName: string;
-  readonly quantity: number;
-  readonly total: number;
-}
-
-export interface IOrderBusinessData {
-  readonly total: number;
-  readonly companyId: string;
-  readonly userId: string;
-  readonly items: readonly IOrderItemBusinessData[];
-}
-
 export async function up(knex: Knex): Promise<any> {
   return knex.schema.createTable(TableName.order, (table) => {
     table.uuid('id').primary().defaultTo(PostgresUUIDV4(knex));
