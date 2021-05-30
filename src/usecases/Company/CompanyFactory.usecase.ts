3ca2ce939b66db1c2ad8607ad26f2718af005691
kelvinfloresta <kelvinf.contato@gmail.com>
2021-05-30 16:46:31 -0300

    refact: remove all use cases factories

--- src/usecases/Company/CompanyFactory.usecase.ts
+++
@@ -1,7 +0,0 @@
-import { CompanyGatewayKnexAdapter } from 'src/adapters/gateways/Company/CompanyKnexAdapter.gateway';
-import { CompanyCase } from './Company.usecase';
-
-export function CompanyCaseFactory() {
-  const gateway = new CompanyGatewayKnexAdapter();
-  return new CompanyCase(gateway);
-}
--- src/usecases/Product/ProductFactory.usecase.ts
+++
@@ -1,7 +0,0 @@
-import { ProductGatewayKnexAdapter } from 'src/adapters/gateways/Product/ProductKnexAdapter.gateway';
-import { ProductCase } from './Product.usecase';
-
-export function ProductCaseFactory() {
-  const productGateway = new ProductGatewayKnexAdapter();
-  return new ProductCase(productGateway);
-}
