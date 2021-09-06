183abc9f74415adb063a4c047d9671bb1c2dbac6
kelvinfloresta <kelvinf.contato@gmail.com>
2021-09-06 12:23:29 -0300

    chore(jest-integration.config): register all dependecies before start

--- jest-integration.config.js
+++ jest-integration.config.js
@@ -1,4 +1,6 @@
 const config = require('./jest.config');
 
 config.testMatch = ['**/*.integration.test.ts'];
+config.setupFiles = ['<rootDir>/setup-integration-test.ts'];
+
 module.exports = config;
---
+++ setup-integration-test.ts
@@ -0,0 +1,3 @@
+import { register } from 'src/adapters/di';
+
+register();
