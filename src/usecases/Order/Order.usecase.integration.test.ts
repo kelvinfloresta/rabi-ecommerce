b684ff4e40dbd50ad7ef4d8f5f83cac8f85fe88f
Kelvin Floresta <kelvinf.contato@gmail.com>
2021-09-12 01:08:15 -0300

    test(Order.usecase): ensure reject if product not found

--- src/usecases/Order/Order.usecase.integration.test.ts
+++ src/usecases/Order/Order.usecase.integration.test.ts
@@ -1,6 +1,8 @@
 import { cleanDatabase, closeDatabase } from 'src/adapters/database/Database.adapter';
 import { container } from 'src/adapters/di';
+import { NotFound } from 'src/errors/NotFound.error';
 import { OrderCase } from 'src/usecases/Order/Order.usecase';
+import { uuidv4 } from 'src/utils/uuid.utils';
 import { createCompanyFixture } from 'src/__fixtures__/company.fixture';
 import { createProductFixture } from 'src/__fixtures__/product.fixture';
 import { createUserFixture } from 'src/__fixtures__/user.fixture';
@@ -35,6 +37,23 @@ describe('File: Order.usecase.ts', () => {
       };
 
       expect(items[0]).toMatchObject(expectedItems);
+    });
+
+    it('Should reject if product not found', async () => {
+      const { id: companyId } = await createCompanyFixture();
+      const userId = await createUserFixture({ companyId });
+
+      const NON_EXISTING_ID = uuidv4();
+      const sut = container.resolve(OrderCase);
+      const promise = sut.create({
+        companyId,
+        userId,
+        items: [{ productId: NON_EXISTING_ID, quantity: 1 }],
+      });
+
+      await expect(promise).rejects.toThrow(
+        new NotFound({ id: NON_EXISTING_ID, resource: 'Product' })
+      );
     });
   });
 });
